3fd5dcaf7d19308f34e106a3dfaf64467c8af64a
Chris Thielen <github@sandgnat.com>
2016-06-29 12:38:32 -0500

    Adding legacy 0.2.10 docs

---
+++ _docs/0.2.10/js/docs-setup.js
@@ -0,0 +1,155 @@
+NG_DOCS={
+  "sections": {
+    "api": "API Reference"
+  },
+  "pages": [
+    {
+      "section": "api",
+      "id": "ui.router",
+      "shortName": "router",
+      "type": "overview",
+      "keywords": "add and angular api app are as dependency for however html include included is js ll main min module modules myapp need needed ng-app only organization other overview purposes router script several src state sub-modules the there this to ui ui-router umbrella var with within your"
+    },
+    {
+      "section": "api",
+      "id": "ui.router.router",
+      "shortName": "router",
+      "type": "overview",
+      "keywords": "angular api app as dependency do in include instead is module not of other overview router sub-module sub-modules this ui util your"
+    },
+    {
+      "section": "api",
+      "id": "ui.router.router.$urlRouter",
+      "shortName": "$urlRouter",
+      "type": "object",
+      "keywords": "$injector $location $locationchangesuccess $on $rootscope $urlrouter address aka allows an and angular api app auth bar by calling change changes config continue custom etc even event evt finally from function halt happens if is logic meetsrequirement method module need object on perform preventdefault proceed protection redirection router run same some starting state sync that the then this to transition triggers ui update url use useful var when with you"
+    },
+    {
+      "section": "api",
+      "id": "ui.router.router.$urlRouterProvider",
+      "shortName": "$urlRouterProvider",
+      "type": "object",
+      "keywords": "$current $injector $location $match $state $stateparams $urlmatcherfactoryprovider $urlrouter $urlrouterprovider according after all allow an and angular another any anytime api app are arguments as behind by can care case changes compiled config configuration configured continue defines didn directly doesn example false find for found function gets given handle handled handler has have here how if in incoming indicate inject injectable insensitive instance interpolated into invalied invoked is it like list make match matches matching methods might module navigable normalized object of on one option or otherwise param params passed path pattern redirect regexp registers replace requested responsibility return returns route router routing rule rules runs scenes services several specific specified specify state string syyntax take takes tells that the them then there through to tolowercase transitionto treated trying two ui until url urlmatcher urls use used useful user using util valid value var version want was watching what when which will you your"
+    },
+    {
+      "section": "api",
+      "id": "ui.router.state",
+      "shortName": "state",
+      "type": "overview",
+      "keywords": "angular api app as dependency do in include instead is main module not of overview router state sub-module the this ui util your"
+    },
+    {
+      "section": "api",
+      "id": "ui.router.state.$state",
+      "shortName": "$state",
+      "type": "object",
+      "keywords": "$current $injector $q $resolve $rootscope $scope $state $statechangestart $statechangesuccess $statenotfound $stateparams $view aborted about absolute accessing active after against aka alias all allows also an ancestor and angular any api app are array as ask associated automatically bar basic be because been between bob broadcast built but called calls changed changestate check checks child com coming common compiled config configs configuration constructed contact contacts controller controllers convenience ctrl current currently custom data defined defines detail details determine differs does easily else equal equality error errors etc even event events everything example examples expect extras failed false fill first fixing for force found from full function generate generation get given globing go going grandchild has have history however href http id if in include includes including inherit inheritance inherited interfaces internally is it item just last letting like list listener ll location lossy low-level map match method methods_go methods_includes missing module most must name navigable need new newer no none not notify now object objects occurred of one ones only options or param parameter parameters params parent parents partial passed path patterns pending person png populate populated possible preventdefault prevented promise provided provides re re-fired re-resolved recommended record reference reinstantiated reinstantiating reject rejection relative reload reloadonsearch reloads replace representing requested required resolve resolves responsible retry return returns right router same search searched section see sent service sets shares sibling similar situations so some soon specific specified specify src started state statename stateorname states strict string success superseded supplied test tested that the them then there they this to toequal toparams transition transitioning transitionto tries true ui unspecified unsuccessfully update url use useful uses util valid values var want well when which will with within work works would you"
+    },
+    {
+      "section": "api",
+      "id": "ui.router.state.$stateProvider",
+      "shortName": "$stateProvider",
+      "type": "object",
+      "keywords": "$current $location $locationprovider $state $statechangeerror $statechangesuccess $stateparams $stateprovider $urlmatcherfactoryprovider $urlrouterprovider abc about above absolute abstract acceptable access accessing action activated activation actual add addition additional aka alias all allows an ancestor and angular any api app application applying arbitrary are array as associated at attach auto-populates autoname based be because become before being belong below beyond builder but by callback called can case chain changed child children clear closest common commonalities config configuration contact contacts content controller controlleras controllerprovider corresponds create created current currently custom data decide declarations declare decorate decorated decorating decoration decorator decorators deeply defined definition dependencies dependency dependent describes dialog directives directly dispatch does don dot down each ensured entered even event every example examples execution existing exited expect explicitly expressions extend extracted factoring factory false features fired fn focuses following for foreach from full func function functionality functions generate given go good has hash have hierarchy home however html id if in inception includes including increasingly inferring inherited injectable injected instance instantiated interdependent interfaces internal internally into introduce is it item itemcontroller its just key like list listcontroller listed looks manually map matches meta-programming model modify multiple name names navigable navigated navigation necessarily needed nest nested never new newest newly no non-deterministic not null object of often on one onenter onexit only opening optional or order original otherwise out over overall overridden override own parameter parameters params parent pass passed passing path peril place populated precedence present primary promise promises properties property provide provider provides published purely receives reference registered registers regular rejected related relevant reload reloadonsearch replace resolve resolved respectively responsible result retrigger return returns root router sales same scope search service set should similar single so some specify state statebuilder stateconfig statename states still string successfully such super super-set takes target template templateprovider templateurl terms test that the them then there these things this though to top-level transitioned treated trigger triggering true two type ui ui-router uiview under unique up url use use-case used useful users using util v1 value values var via view viewname views wait way we were what when where which will with within without works would xyz you your"
+    },
+    {
+      "section": "api",
+      "id": "ui.router.state.$uiViewScroll",
+      "shortName": "$uiViewScroll",
+      "type": "object",
+      "keywords": "$anchorscroll $timeout $uiviewscroll $uiviewscrollprovider anchor api be by called calling can dom element enabled has if into it jqlite methods_useanchorscroll object on prefer refresh rely router scroll scrolls so state the this time to ui useanchorscroll view when with you"
+    },
+    {
+      "section": "api",
+      "id": "ui.router.state.$uiViewScrollProvider",
+      "shortName": "$uiViewScrollProvider",
+      "type": "object",
+      "keywords": "$anchorscroll $uiviewscroll $uiviewscrollprovider anchor angularjs api back based core for function object on org provider returns reverts router scrolling service state that the to ui url useanchorscroll using"
+    },
+    {
+      "section": "api",
+      "id": "ui.router.state.$view",
+      "shortName": "$view",
+      "type": "object",
+      "keywords": "$rootscope $templatefactory $view api function load name object option options router state ui util"
+    },
+    {
+      "section": "api",
+      "id": "ui.router.state.directive:ui-sref",
+      "shortName": "ui-sref",
+      "type": "directive",
+      "keywords": "$state $timeout about absolute also an and any api are associated attribute automatically aware be binds browser by can clicking compile compiled contact contacts containing ctrl-clicking detail directive example following generate go handled has have here home how href html html5mode id if in inherit is it just like link lives loaded location method methods_href middle-clicking name natively need ng-repeat of off on optional options or other parameters pass passed path paths relative reload restricted right-clicking router specify state statename tag template that the then to transition trigger true ui ui-sref ui-sref-opts update url use using valid via will with within words would you"
+    },
+    {
+      "section": "api",
+      "id": "ui.router.state.directive:ui-sref-active",
+      "shortName": "ui-sref-active",
+      "type": "directive",
+      "keywords": "$interpolate $state $stateparams active add alongside an and api app appear appearance are as be bilbobaggins button by changes class class1 class2 class3 classes contains different directive directives distinguishing during element following format from further given having href html ignored in inactive interpolated is it item items link may menu menus multiple name navigation of on parameter primary related relying removing resulting router simplify space-separated special specified state template the them time to ui ui-sref ui-sref-active use-case user value when will with working"
+    },
+    {
+      "section": "api",
+      "id": "ui.router.state.directive:ui-view",
+      "shortName": "ui-view",
+      "type": "directive",
+      "keywords": "$anchorscroll $compile $controller $document $injector $state $stateprovider $uiviewscroll $uiviewscrollprovider above activation allows amongst an and angularjs any api are autoscroll back be behavior browser but by call can case chart compelling config convenient could custom data default different directive during eca elements empty equivalent evaluate evaluates examples explicitly expression false for functionality have home html if in into is it its let like live ll main more multiple name named no not of old one onload only or org other overridden place populate populated present property really reason revert root router same scopevariable scroll service set shortcut should single so specifying state states tells template templates than that the then there they this though to true typically ui ui-router ui-view unique unnamed up updates use useanchorscroll using valid view viewname views wanted when whenever where window with within you your"
+    },
+    {
+      "section": "api",
+      "id": "ui.router.state.filter:includedByState",
+      "shortName": "includedByState",
+      "type": "filter",
+      "keywords": "$state api filter fullorpartialstatename includes methods_includes router state to translates ui"
+    },
+    {
+      "section": "api",
+      "id": "ui.router.state.filter:isState",
+      "shortName": "isState",
+      "type": "filter",
+      "keywords": "$state api filter is methods_is router state statename to translates ui"
+    },
+    {
+      "section": "api",
+      "id": "ui.router.util",
+      "shortName": "util",
+      "type": "overview",
+      "keywords": "angular api app as dependency do in include instead is module not of other overview router sub-module sub-modules this ui util your"
+    },
+    {
+      "section": "api",
+      "id": "ui.router.util.$resolve",
+      "shortName": "$resolve",
+      "type": "object",
+      "keywords": "$injector $q $resolve $routeprovider after all allows an ancestor and another any api arbitrary are as available backwards-compatibility be been between but by call called calls can case caues cause contains cyclic decorated depend dependencies dependency directly eagerly efficient either equivalent error even exception fact fetch for former from fulfilled function functions further get graphs has have if immediately in inherited injectable injectables instead internally invocable invocables invoke invoked is it just likely likewise local locals make manages methods missing more multiple name no not note number object objects of on once or order other parameter parent part pass passed permitted precedence primarily promise promises propagated property rather recursively rejected rejection resolution resolve resolved resolves resulting return returned router routes same see self service services set soon special specified string studies study supported taken than that the this throw throws times to true ui unlike used util value values via well which will with would"
+    },
+    {
+      "section": "api",
+      "id": "ui.router.util.$templateFactory",
+      "shortName": "$templateFactory",
+      "type": "object",
+      "keywords": "$http $injector $templatecache $templatefactory an and api are as by config configuration configured create creates defaults defined first following for from fromconfig fromprovider fromstring fromurl function html if in injectable invoke invoking is load loaded loading loads locals manages no null object of one or order parameters params pass promise properties provider returning returns router search service specified string template templateprovider templates templateurl that the to ui url used util via which"
+    },
+    {
+      "section": "api",
+      "id": "ui.router.util.$urlMatcherFactory",
+      "shortName": "$urlMatcherFactory",
+      "type": "object",
+      "keywords": "$urlmatcherfactory $urlmatcherfactoryprovider against also and api available be behavior case caseinsensitivematch check compile concat creates default define exec factory false following for format function functions has if in instances is ismatcher manner match matching name not object or otherwise pattern perform providers returns router sensistive sensitive should specified the to true type ui under url urlmatcher util value"
+    },
+    {
+      "section": "api",
+      "id": "ui.router.util.type:UrlMatcher",
+      "shortName": "UrlMatcher",
+      "type": "object",
+      "keywords": "$location adding against all allows always an and any api append appending are array as backslash backward be bob both brace braces but by can captured captures case caseinsensitivematch catch-all characters colon compatibility compile concat concatenated consists constructed contain containing contains contructor creates creating curly current default determine digits ditto do does empty entire equivalent escaped even exactly example examples exec explicit expression extracts false followed following for format formatted from function guarantees has have hello hex href id if in influence insensitive into is it itself just letters list match matched matcher matchers matches matching may means mentioned methods_exec modified multiple must name named names new no non-null not null number object of only optional optionally or order other otherwise pairs parameter parameters part passed path pattern patterns placeholder placeholders portion prefix present previous property read-only regex regexp regular relative rest returned returns router same search searchparams second segment separated should similar simple slashes source sourcepath sourcesearch special specified start starting static string strings substitute substituting suffixes syntax tests than that the their there they this through time to trailing treated treatment true two type ui underscore understood unique unspecified url urlmatcher urls used using util value values was when whether which will with within without word yes"
+    }
+  ],
+  "apis": {
+    "api": true
+  },
+  "html5Mode": false,
+  "startPage": "/api/ui.router",
+  "scripts": [
+    "angular.min.js"
+  ]
+};
