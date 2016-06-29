69d49401f6124278c5a68d60f66542560fc4b0fe
Chris Thielen <github@sandgnat.com>
2016-06-29 16:56:32 -0500

    docs(legacy): Fix legacy docs from 0.2.15+ to work in jekyll

--- _docs/process_docs.sh
+++ _docs/process_docs.sh
@@ -1,13 +1,20 @@
 #!/bin/sh
 # Find legacy docs and frame them
-for index in `ls */index.html` ; do
-  if grep -q ".title ng-bind-template=.UI Router: {{partialTitle}}..Docs..title." $index ; then
-    echo "processing legacy docs: $index";
-    mv "$index" `dirname $index`/index.htm;
-    cp ../assets/legacy.docs.frame.html `dirname $index`/index.html;
+for file in `ls */index.html` ; do
+  if grep -q ".title ng-bind-template=.UI Router: {{partialTitle}}..Docs..title." $file ; then
+    if head -n 1 $file | grep -q "^---$" ; then
+      echo "skipping $file";
+    else
+      echo "processing legacy docs: $file";
+      (echo "---"; \
+       echo "---"; \
+       echo "{% raw %}"; \
+       cat $file | sed -e 's/navbar-fixed-top//g' -e 's/class="brand">UI Router/ \
+       echo "{% endraw %}"; \
+      ) > $file.tmp &&  mv -f $file.tmp $file;
+    fi
   fi
 done
-
 
 # Add jekyll --- --- header--- to all html files, so jekyll will process them
 for file in `find . -name "*.html" | grep -v "/partials/"` ; do
