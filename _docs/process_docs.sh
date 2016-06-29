5764e4648906c810fa0e03188d8b7c3e29f20f56
Chris Thielen <github@sandgnat.com>
2016-06-29 11:00:17 -0500

    Add legacy 0.2.18 docs

---
+++ _docs/process_docs.sh
@@ -0,0 +1,22 @@
+#!/bin/sh
+# Find legacy docs and frame them
+for index in `ls */index.html` ; do
+  if grep -q ".title ng-bind-template=.UI Router: {{partialTitle}}..Docs..title." $index ; then
+    echo "processing legacy docs: $index";
+    mv "$index" `dirname $index`/index.htm;
+    cp legacy.frame.htm `dirname $index`/index.html;
+  fi
+done
+
+
+# Add jekyll --- --- header--- to all html files, so jekyll will process them
+for file in `find . -name "*.html" | grep -v "/partials/"` ; do
+  if head -n 1 $file | grep -q "^---$" ; then
+      echo "skipping $file";
+  else
+      echo "processing $file";
+      (echo "---"; \
+       echo "---"; cat $file \
+      ) > $file.tmp &&  mv -f $file.tmp $file;
+  fi
+done
