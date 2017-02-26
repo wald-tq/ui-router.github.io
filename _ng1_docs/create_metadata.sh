58f3b701c677a25529e39b8a1ec3c823298558e8
Chris Thielen <github@sandgnat.com>
2017-02-26 17:28:23 -0600

    Add ngdoc_assets from legacy docs

--- _ng1_docs/create_metadata.sh
+++ _ng1_docs/create_metadata.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 pushd `dirname $0`;
 
-find . -type d -depth 1 | sort | node ./paths_as_json_array.js > versions.json
+find . -type d -depth 1 | sort | grep -v "ngdoc" | node ./paths_as_json_array.js > versions.json
 
 for i in `find . -type d -depth 1` ; do
   pushd $i;
